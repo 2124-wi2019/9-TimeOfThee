6a1442b63234cb844b11a07cd13ce67dbd1c94cd
Timothy <timeofthee@gmail.com>
2020-02-11 18:59:39 -0600

    Uploading Assignment 9
    title

---
+++ washington_a09b.js
@@ -0,0 +1,12 @@
+/*
+Timothy Washington
+washington_a09b.js
+INFO 2124 - Javascript I
+Thoendel
+Creation date: Feb 11, 2020
+*/
+console.log("<p> tag count: "+document.getElementsByTagName("p").length);//say
+console.log("<div> tag count: "+document.getElementsByTagName("div").length);//say
+console.log(".paragraph class count: "+document.getElementsByClassName("paragraph").length);//say
+console.log(".block-text class count: "+document.getElementsByClassName("block-text").length);//say
+console.log("element b001: "+document.getElementById("b001").textContent);//say
